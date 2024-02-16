be28e68065a3ab94be1deeb6b3e3a75f7e4fde96
Harshit Verma <harshitverma1996@yahoo.com>
2024-02-16 22:42:13 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -11,12 +11,10 @@ import{getStorage} from'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFL-VpMEXPpDDOS-A7-mOzZWfdwnOFQGo",
   authDomain: "vaarta-23463.firebaseapp.com",
   projectId: "vaarta-23463",
   storageBucket: "vaarta-23463.appspot.com",
   messagingSenderId: "496119735414",
-  appId: "1:496119735414:web:5a837c6aad038112aceb57"
 };
 
 // Initialize Firebase
